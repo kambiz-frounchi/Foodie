037dcd70cd6c9e4ee32dd8f274a36b584de3a668
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-10-19 20:38:11 -0400

    added react pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Feed from './pages/Feed';
+import FindRecipe from './pages/FindRecipe';
+import PostRecipe from './pages/PostRecipe';
+import Recipe from './pages/Recipe';
+import UserProfile from './pages/UserProfile';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Header from "./components/Header/index";
 
 function App() {
   return (
+    <Router>
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path={["/"]}>
+            <Feed/>
+          </Route>
+          <Route exact path={["/recipe/:id"]}>
+            <Recipe/>
+          </Route>
+          <Route exact path={["/user"]}>
+            <UserProfile/>
+          </Route>
+          <Route exact path={["/find"]}>
+            <FindRecipe/>
+          </Route>
+          <Route exact path={["/post"]}>
+            <PostRecipe/>
+          </Route>
+          <Route exact path={["/signup"]}>
+            <Signup/>
+          </Route>
+          <Route exact path={["/login"]}>
+            <Login/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+/*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +63,4 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+*/
--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 
-function Recipe () {
+function Feed () {
     return null;
 }
 
-export default Recipe;
+export default Feed;
