e7ab23ac92eec333faf42546d47d3df885f87ce2
Adam Tatar <adam@wikileaf.com>
2020-10-28 19:09:08 -0400

    added post recipe function

--- client/src/pages/PostRecipe.js
+++ client/src/pages/PostRecipe.js
@@ -2,6 +2,25 @@ import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 
 function PostRecipe () {
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (formObject.userId) {
+          API.postRecipe({
+            userId: formObject.userId,
+            createdDate: ?
+            name: formObject.name,
+            image: formObject.image,
+            time: formObject.time,
+            difficulty: formObject.difficulty,
+            cuisine: formObject.cuisine.type,
+            ingredients: formObject.ingredients,
+            tags: formObject.tags
+
+          })
+            .then(res => loadRecipes())
+            .catch(err => console.log(err));
+        }
+      };
     return null;
 }
 
