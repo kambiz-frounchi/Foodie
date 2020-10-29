c76c5a3f765a3a479c6e121c5b8e50be40baa92d
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-10-28 20:25:18 -0400

    add first draft of FeedRecipe component (looks ugly)

---
+++ client/src/components/FeedRecipe/index.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Button, Card } from "react-bootstrap";
+
+function FeedRecipe(props) {
+    return (<div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="" alt="recipe image" />
+        <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">{props.email}</Card.Subtitle>
+            <Card.Text>
+                {props.description}
+            </Card.Text>
+            <Button variant="primary">Like</Button>
+        </Card.Body>
+        </Card>            
+    </div>);
+}
+
+export default FeedRecipe;
