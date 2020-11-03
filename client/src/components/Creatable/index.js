636f6c7ca12d331c0b4e7f6fe237960d1262c9e3
Mattias Besemann <mattias.besemann@gmail.com>
2020-11-02 22:42:40 -0500

    Update creatable control

--- client/src/components/Creatable/index.js
+++ client/src/components/Creatable/index.js
@@ -20,7 +20,6 @@ export default function Creatable(props) {
 
   return (
     <div className={classes.root}>
-<<<<<<< HEAD
       <Autocomplete
         // getOptionSelected={(option, value) => option.name == value.name}
         multiple
@@ -28,30 +27,12 @@ export default function Creatable(props) {
         options={props.items}
         value={props.value}
         getOptionLabel={(option) => option.name}
-=======
-      <TextField
-        id="standard-basic"
-        label="Ingredient"
-        onChange={props.onInputChange}
-      />
-      <Button variant="contained" color="primary" onClick={props.onClick}>
-        Add
-      </Button>
-      <Autocomplete
-        multiple
-        id="tags-outlined"
-        options={props.items}
-        value={props.added}
-        getOptionLabel={(option) => option.name}
-        // defaultValue={[top100Films[13]]}
->>>>>>> d5a913ae67adda23e84b2203ce6d84c5fc8924ff
         filterSelectedOptions
         onChange={props.onChange}
         renderInput={(params) => (
           <TextField
             {...params}
             variant="outlined"
-<<<<<<< HEAD
             label={`${props.label}s`}
             placeholder={props.label}
           />
@@ -69,13 +50,6 @@ export default function Creatable(props) {
           Add
         </Button>
       </div>
-=======
-            label={props.label}
-            placeholder="Favorites"
-          />
-        )}
-      />
->>>>>>> d5a913ae67adda23e84b2203ce6d84c5fc8924ff
     </div>
   );
 }
