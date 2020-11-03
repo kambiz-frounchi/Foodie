abf53be50ede1585415b2f38dc29e1c2c13241d4
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-11-02 21:27:53 -0500

    add support for getting images in the recipeFeed component

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -2,50 +2,31 @@ const router = require("express").Router();
 const recipesController = require("../../controllers/recipesController");
 const isAuthenticated = require("../../passport/isAuthenticated");
 const path = require("path");
+const fs = require("fs");
 
-router.route("/find")
-  .post(recipesController.find);
+router.route("/find").post(recipesController.find);
 
-router.route("/")
-  .get(recipesController.findAll)
-  .post(recipesController.create);
+router.route("/").get(recipesController.findAll).post(recipesController.create);
 
-router.route("/upload")
-  .post(async (req, res) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send("No files were uploaded.");
+router.route("/upload").post((req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  console.log(req.files);
+  const recipeImageFile = req.files.myimage;
+  recipeImageFile.mv(
+    path.join(__dirname, "../../images/", recipeImageFile.name),
+    (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err);
+      }
     }
-    console.log(req.files);
-    //const avatar = req.files.avatar;
-    //console.log(avatar);
-    //avatar.name = "avatar.jpg";
-    const recipeImageFile = req.files.myimage;
-    //console.log(avatar);
-    // Use the mv() method to place the file somewhere on your server
-    /*
-    await avatar.mv(path.join(__dirname, "../../images/", avatar.name), err => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send(err);
-      }
-    });
-    */
+  );
+  res.status(200).send("file received");
+});
 
-    await recipeImageFile.mv(path.join(__dirname, "../../images/", recipeImageFile.name), err => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send(err);
-      }
-    });
-
-
-
-    //console.log("upload");
-    //console.log(req.body);
-    //console.log(req.body);
-    //res.json({msg: "file received"});
-    res.status(200).send("");
-  })
+router.route("/images/:id").get(recipesController.getImage);
 
 // Matches with "/api/recipes"
 //router.route("/")
--- server.js
+++ server.js
@@ -20,6 +20,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(express.static("public"));
+
 app.use(morgan('dev'));
 app.use(
 	bodyParser.urlencoded({
