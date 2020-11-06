b9768afa16109b4c70c21d9f9d91b53a4d7d7cc5
Mattias Besemann <mattias.besemann@gmail.com>
2020-11-06 12:53:32 -0500

    Fix back button

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Header from "../Header";
+import {useHistory} from "react-router-dom";
 import { ListGroup, Badge } from "react-bootstrap";
 
 export default function RecipeContent(props) {
+  const history = useHistory();
+
   if (!props.ingredients) return null;
   if (!props.tags) return null;
   if (!props.cuisines) return null;
 
   return (
     <div>
-      <a href="/"><h6><i class="fas fa-chevron-left fa-2x"></i>&nbsp; Back</h6></a>
+      <a href="#" onClick={() => history.goBack()}><h6><i class="fas fa-chevron-left fa-2x"></i>&nbsp; Back</h6></a>
       <br/><br/>
       <h1>{props.name}</h1>
       <h6>By {props.nickname}</h6>
@@ -20,11 +23,11 @@ export default function RecipeContent(props) {
       ></img>
       <p>{props.description}</p>
       <Badge pill variant="secondary">
-        {props.time}
+        Time: {props.time} mins
       </Badge>
       &nbsp;
       <Badge pill variant="info">
-        {props.difficulty}
+        Difficulty: {props.difficulty}
       </Badge>
       &nbsp;
       <Badge pill variant="primary">
