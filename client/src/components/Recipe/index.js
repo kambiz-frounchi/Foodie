435c39284acb5460ac1bb111c737ea2a922d7580
Mattias Besemann <mattias.besemann@gmail.com>
2020-11-06 12:41:31 -0500

    Make recipe page fetch data and add links to feed

--- client/src/components/Recipe/index.js
+++ client/src/components/Recipe/index.js
@@ -3,32 +3,56 @@ import Header from "../Header";
 import { ListGroup, Badge } from "react-bootstrap";
 
 export default function RecipeContent(props) {
-    return(
-        <div>
-            <h1>{props.name}</h1>
-            <img src="{props.image}" alt="Recipe Name"></img>
-            <p>Recipe Description</p>
-            <Badge pill variant="light">Recipe Time</Badge>{' '}
-            <Badge pill variant="info">Recipe Difficulty</Badge>{' '}
-            <Badge pill variant="primary">Recipe Category</Badge>{' '}
-            <h3>Ingredients:</h3>
-            <ListGroup>
-            <ListGroup.Item>Ingredient</ListGroup.Item>
-            <ListGroup.Item>Ingredient</ListGroup.Item>
-            <ListGroup.Item>Ingredient</ListGroup.Item>
-            <ListGroup.Item>Ingredient</ListGroup.Item>
-            <ListGroup.Item>Ingredient</ListGroup.Item>
-            </ListGroup>
-            <h3>Tags:</h3>
-            <ListGroup>
-            <ListGroup.Item>Tag</ListGroup.Item>
-            <ListGroup.Item>Tag</ListGroup.Item>
-            <ListGroup.Item>Tag</ListGroup.Item>
-            <ListGroup.Item>Tag</ListGroup.Item>
-            <ListGroup.Item>Tag</ListGroup.Item>
-            </ListGroup>
-        </div>
-    )
+  if (!props.ingredients) return null;
+  if (!props.tags) return null;
+  if (!props.cuisines) return null;
 
+  return (
+    <div>
+      <a href="/"><h6><i class="fas fa-chevron-left fa-2x"></i>&nbsp; Back</h6></a>
+      <br/><br/>
+      <h1>{props.name}</h1>
+      <h6>By {props.nickname}</h6>
+      <img
+        width="500"
+        src={`/api/recipes/images/${props.img}`}
+        alt={props.name}
+      ></img>
+      <p>{props.description}</p>
+      <Badge pill variant="secondary">
+        {props.time}
+      </Badge>
+      &nbsp;
+      <Badge pill variant="info">
+        {props.difficulty}
+      </Badge>
+      &nbsp;
+      <Badge pill variant="primary">
+        {props.cuisine}
+      </Badge>
+      <br/><br/>
+      <h6>Cuisines:</h6>
+      <ListGroup>
+        {props.cuisines.map((cuisine) => (
+          <ListGroup.Item key={cuisine._id}>{cuisine.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+      <br/>
+      <h6>Ingredients:</h6>
+      <ListGroup>
+        {props.ingredients.map((ingredient) => (
+          <ListGroup.Item key={ingredient._id}>
+            {ingredient.name}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <br/>
+      <h6>Tags:</h6>
+      <ListGroup>
+        {props.tags.map((tag) => (
+          <ListGroup.Item key={tag._id}>{tag.name}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
+  );
 }
-
